5d85d25563edfad631d8f02da473a46ecc11adfb
captmorgan <tekintl@gmail.com>
2017-06-22 10:21:10 -0400

    Update mount.cs

--- mount.cs
+++ mount.cs
@@ -1,5 +1,6 @@
 public override void PreFight() 
 {
+	//remove the mount buff when approaching target
 	if (this.Player.GotBuff("Swift Palomino") && this.Target.DistanceToPlayer <= 25)
 	{
 	  this.Player.UseItem("Swift Palomino");
@@ -9,6 +10,7 @@ public override void PreFight()
 
 public override void Fight() 
 {
+	// remove mount buff if in combat
   if (this.Player.GotBuff("Swift Palomino"))
 	{
     this.Player.UseItem("Swift Palomino");
@@ -17,8 +19,11 @@ public override void Fight()
 
 public override bool Buff() 
 {
+	// Dont mount until looting is complete (must have looting turned on)
 	if (Player.NeedToLoot()) return true;
+	// Dont break casting
 	if (this.Player.IsCasting == "Swift Palomino") return false;
+	// use mount if target is far away, or have not gotten a new target. testing other meathods.
 	if (!this.Player.GotBuff("Swift Palomino") && this.Target.DistanceToPlayer >= 40)
   {
 		this.Player.UseItem("Swift Palomino");
