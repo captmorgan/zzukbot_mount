4aa1db9621b90b9e5142770c8c70904f968e9023
captmorgan <tekintl@gmail.com>
2017-06-22 10:13:14 -0400

    Create mount.cs

---
+++ mount.cs
@@ -0,0 +1,28 @@
+public override void PreFight() 
+{
+  if (this.Player.GotBuff("Swift Palomino") && this.Target.DistanceToPlayer <= 25)
+	{
+	  this.Player.UseItem("Swift Palomino");
+	}
+  else this.Player.Cast("Frostbolt");
+}
+
+public override void Fight() 
+{
+  if (this.Player.GotBuff("Swift Palomino"))
+	{
+    this.Player.UseItem("Swift Palomino");
+  }
+}
+
+public override bool Buff() 
+{
+	  if (Player.NeedToLoot()) return true;
+	  if (this.Player.IsCasting == "Swift Palomino") return false;
+	  if (!this.Player.GotBuff("Swift Palomino") && this.Target.DistanceToPlayer >= 40)
+  {
+		this.Player.UseItem("Swift Palomino");
+		return false;
+	}
+	 return true;
+}
